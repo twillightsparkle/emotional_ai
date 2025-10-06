c69e4322df302f98f9afeec81bb244995693c76d
unknown <Thang.Tran@student.oulu.fi>
2025-10-06 13:21:34 +0300

    change background

--- src/components/Background.jsx
+++ src/components/Background.jsx
@@ -3,7 +3,7 @@ import { TextureLoader } from 'three'
 import * as THREE from 'three'
 
 export function Background() {
-  const texture = useLoader(TextureLoader, '/texture/Hirsipaviljonki_pieni.jpg')
+  const texture = useLoader(TextureLoader, '/texture/aito.jpg')
   
   return (
     <mesh position={[0, 5, -10]} scale={[80, 48, 4]}>
